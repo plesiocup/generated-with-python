1132f84b0f072f8ecd0dc879abe0cfbe1794945b
t_takumaru <t.takuma.saga.u@gmail.com>
2023-12-16 17:34:38 +0900

    タイトルから説明文が生成されるようになった、説明文から画像を生成したい

--- oai_create_image.py
+++ oai_create_image.py
@@ -8,17 +8,17 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 class Oai_create_image:
 
-    def get_response(self, title):
+    def get_response(self, element):
 
-        prompt = '' + title + '' # 良さげな画像になるようにpromptの文章をそのうち考える
+        # prompt = '' + element + '今の文章が説明文が映画の感動的なシーン' # 良さげな画像になるようにpromptの文章をそのうち考える
 
         response = openai.Image.create(
-            prompt = prompt,
+            prompt = element, # そのうちpromptに差し替える
             size = '1024x1024',
             n = 1
         )
         image_url = response["data"][0]["url"]
 
-        print('________________________\n__get_response_success__\n________________________\n')
+        # print('________________________\n__get_response_success__\n________________________\n')
 
         return image_url
